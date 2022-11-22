7855f676ddbfcd8c2474578319524d5e689f205d
MinamiSawamura <c0a2106328@edu.teu.ac.jp>
2022-11-22 15:56:56 +0900

    クイズ完成

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,13 +1,22 @@
 import random
 
-number = random.randint(0, 2)
+def shutudai(qa_lst):
+    qa = random.choice(qa_lst)
+    print("問題：" + qa["q"])
+    return qa["a"]
 
-quiz_list = ["サザエの旦那の名前は？", "カツオの妹の名前は？", "タラオはカツオから見てどんな関係？"]
-print("問題：")
-print(quiz_list[number])
-answer = input("答えを入力してください：")
-quiz_answer = [["マスオ", "ますお"], ["ワカメ", "わかめ"], ["甥", "おい", "甥っ子", "おいっこ"]]
-if answer in quiz_answer[number]:
-    print("正解！！！")
-else:
-    print("不正解です･･･")
+def kaitou(ans_lst):
+    ans = input("答えを入力してください：")
+    if ans in ans_lst:
+        print("正解！！！")
+    else:
+        print("不正解です･･･")
+
+if __name__ == "__main__":
+    qa_lst = [
+        {"q":"サザエの旦那の名前は？", "a":["マスオ", "ますお"]}, 
+        {"q":"カツオの妹の名前は？", "a":["ワカメ", "わかめ"]},
+        {"q":"タラオはカツオから見てどんな関係？", "a":["甥", "おい", "甥っ子", "おいっこ"]}
+    ]
+    ans_lst = shutudai(qa_lst)
+    kaitou(ans_lst)
