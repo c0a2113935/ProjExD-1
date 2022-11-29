1b887c8af81d7a90c07db498b721f6ae8d54d4e7
MinamiSawamura <c0a2106328@edu.teu.ac.jp>
2022-11-29 14:34:36 +0900

    ４回目

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,14 +9,17 @@ def msg(event):
 root = tk.Tk()
 root.geometry("300x500")
 
-r, c = 0, 0
+entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
+entry.grid(row=0, column=0, columnspan=3)
+
+r, c = 1, 0
 for num in range(9, -1, -1):
-    if c == 3:
-        c = 0
-        r += 1
     Button = tk.Button(root, text=f"{num}", width=4, height=2, font=("Times New Roman", 30))
     Button.grid(row=r, column=c)
     Button.bind("<1>", msg)
     c += 1
+    if c%3 == 0:
+        c = 0
+        r += 1
 
 root.mainloop()
