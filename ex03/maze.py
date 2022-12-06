e50c99b3f5f7271defe5381ed66c1e982db9f766
MinamiSawamura <c0a2106328@edu.teu.ac.jp>
2022-12-06 14:30:45 +0900

    練習５

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -11,6 +15,6 @@ if __name__ == "__main__":
     canvas.create_image(cx, cy, image=img, tag="kokaton")
 
     key = ""
-
+    root.bind("<KeyPress>", key_down)
     canvas.pack()
     root.mainloop()
