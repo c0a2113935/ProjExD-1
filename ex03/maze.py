35e4bd810d1f6289f298a6e7da99e2785492d8ba
MinamiSawamura <c0a2106328@edu.teu.ac.jp>
2022-12-06 14:26:06 +0900

    練習３

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,5 +4,11 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+
+    cx = 300 #横座標
+    cy = 400 #縦座標
+    img = tk.PhotoImage(file="fig/8.png")
+    canvas.create_image(cx, cy, image=img, tag="kokaton")
+    
     canvas.pack()
     root.mainloop()
