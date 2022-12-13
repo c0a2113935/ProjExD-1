368dce266dc98df32e3bb8a9ec6fd93d2b3eb71f
MinamiSawamura <c0a2106328@edu.teu.ac.jp>
2022-12-13 14:17:51 +0900

    練習２

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,12 +2,21 @@ import pygame as pg
 import sys
 
 def main():
+    clock = pg.time.Clock()
     # 練習１
     pg.display.set_caption("逃げろ！こうかとん")
-    scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_sfc = pg.display.set_mode((1600/2, 900/2))
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+    pgbg_rct = pgbg_sfc.get_rect()
 
-
+    # 練習２
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        pg.display.update()
+        clock.tick(1000)
+        
 
 
 if __name__ == "__main__":
